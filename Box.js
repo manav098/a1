b54b0c71f8f7ca3701c74ab273b0de1acdac7348
manav098 <79452617+manav098@users.noreply.github.com>
2021-03-15 16:56:12 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,28 @@
+class Box{
+    constructor(x,y,width,height){
+        var options={
+            'restitution':1
+
+        }
+
+        this.body=Bodies.rectangle(x,y,width,height,options)
+        this.width=width
+        this.height=height
+
+        World.add(world,this.body)
+    }
+    
+display(){
+    var p=this.body.position;
+    var angle=this.body.angle
+    push ();
+    translate (p.x,p.y)
+    rotate (angle)
+    rectMode(CENTER)
+
+    fill(255)
+    rect(0,0,this.width,this.height)
+    pop ()
+}
+
+}
